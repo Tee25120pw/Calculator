fb79b709f6353bdd4024462ce99a8edd8725cd0b
Woramet Koonchalakorn <102159551+Tee25120pw@users.noreply.github.com>
2023-02-18 19:11:10 +0700

    [Update] branch

--- app/src/main/java/academy/learnprogramming/calculator/MainActivity.kt
+++ app/src/main/java/academy/learnprogramming/calculator/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             newNumber.append(b.text)
         }
         
-        // update from master
+        // update from master branch
 
         button0.setOnClickListener(listener)
         button1.setOnClickListener(listener)
